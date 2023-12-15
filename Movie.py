384f8f406d2a0bd56a85fb339dea7ea08865287d
Givomir <stanislav.peev91@gmail.com>
2023-12-15 21:01:06 +0200

    Commit movsrch functionality and made corrections in movie database according to the project defenition.

--- Movie.py
+++ Movie.py
@@ -1,8 +1,14 @@
 # movie.py
-
 class Movie:
-    def __init__(self, title, release_year, genre, rating):
+    def __init__(self, id, title, description, release_year, genre, rating, favorite):
+        self.id = id
         self.title = title
+        self.description = description
         self.release_year = release_year
         self.genre = genre
         self.rating = rating
+        self.favorite = favorite
+
+    def __str__(self):
+        return f"{self.id} - {self.title} - {self.description} - {self.release_year} - {self.genre} - {self.rating} - {self.favorite}"
+
