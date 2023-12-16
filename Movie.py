861229b6015d504c6468707f6abee38e2b3fbc97
Stanislav Peev <147485223+Givomir@users.noreply.github.com>
2023-12-16 19:00:22 +0200

    Revert "Commit movsrch functionality and made corrections in movie database a…"

--- Movie.py
+++ Movie.py
@@ -1,14 +1,8 @@
 # movie.py
+
 class Movie:
-    def __init__(self, id, title, description, release_year, genre, rating, favorite):
-        self.id = id
+    def __init__(self, title, release_year, genre, rating):
         self.title = title
-        self.description = description
         self.release_year = release_year
         self.genre = genre
         self.rating = rating
-        self.favorite = favorite
-
-    def __str__(self):
-        return f"{self.id} - {self.title} - {self.description} - {self.release_year} - {self.genre} - {self.rating} - {self.favorite}"
-
--- main.py
+++ main.py
@@ -1,60 +1,30 @@
 # main.py
-import click
+import argparse
 from Movie import Movie
 from movie_database import MovieDatabase
 
-@click.group()
-def cli():
-    pass
-
-@cli.command()
-@click.option("--id", required=False, help="ID for the movie")
-@click.option("--title", required=True, help="Title of the movie")
-@click.option("--description", required=True, help="Description of the movie")
-@click.option("--release-year", required=True, help="Release year of the movie")
-@click.option("--genre", required=True, help="Genre of the movie")
-@click.option("--rating", required=True, help="Rating of the movie")
-@click.option("--favorite", required=False, help="User preference for this movie")
-def movadd(id, title, description, release_year, genre, rating, favorite):
-    """Add a new movie."""
-    movie = Movie(id, title, description, release_year, genre, rating, favorite)
-    movie_db.add_movie(movie)
-    click.echo("Movie added successfully.")
-
-@cli.command()
-def movlst():
-    """List all movies."""
-    movie_db.list_movies()
-
-@click.command()
-@click.argument('movie_id', required=True)
-def movdt(movie_id):
-    """Get details of a movie."""
-    movie_details = movie_db.get_movie_by_id(movie_id)
-    if movie_details:
-        click.echo(f"Movie Details: {movie_details}")
-    else:
-        click.echo(f"Movie with ID {movie_id} not found.")
-
-@click.command()
-@click.argument('query', required=True)
-def movsrch(query):
-    """Search for movies based on their titles."""
-    search_results = movie_db.search_movies(query)
-    if search_results:
-        click.echo("Search Results:")
-        for result in search_results:
-            click.echo(result)
-    else:
-        click.echo(f"No movies found with title containing '{query}'.")
-
-
-# Add the  command to the CLI
-cli.add_command(movadd)
-cli.add_command(movlst)
-cli.add_command(movdt)
-cli.add_command(movsrch)
-
 if __name__ == "__main__":
     movie_db = MovieDatabase("movie_database.db")
-    cli()
+
+    parser = argparse.ArgumentParser(description="Movie Database CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    # Add the 'add-movie' command
+    add_movie_parser = subparsers.add_parser("movadd", help="Add a new movie")
+    add_movie_parser.add_argument("--title", required=True, help="Title of the movie")
+    add_movie_parser.add_argument("--release-year", required=True, help="Release year of the movie")
+    add_movie_parser.add_argument("--genre", required=True, help="Genre of the movie")
+    add_movie_parser.add_argument("--rating", required=True, help="Rating of the movie")
+
+    # Add the 'list-movies' command
+    subparsers.add_parser("movlst", help="List all movies")
+
+    args = parser.parse_args()
+
+    if args.command == "movadd":
+        movie = Movie(args.title, args.release_year, args.genre, args.rating)
+        movie_db.add_movie(movie)
+    elif args.command == "movlst":
+        movie_db.list_movies()
+    else:
+        print("Invalid command. Use 'movadd' or 'movlst'.")
